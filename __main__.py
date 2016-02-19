9509cc7ac44b5bba649af71d4e23f5b7a09322fa
Matt Blanchard <mblanchard23@gmail.com>
2016-02-19 15:57:01 +0000

    Where'd my files go

--- __main__.py
+++ __main__.py
@@ -1,9 +1,33 @@
-def lst2xl(lst,xlfile):
-	rows = len(lst)
-	if rows == 0:
-		print 'Error: List is empty. Nothing done.'
+from openpyxl import utils
+
+def lst2xl(lst,xlws,start_cell):
+	
+
+	row_count = len(lst)
+	
+	if not row_count: # Do nothing if data set is empty
 		return None
 
-	cols = len(lst[0])
+
+	col_count = len(lst[0])
+	
+
+	row_start = ws[start_cell].row 
+	col_start = column_index_from_string(ws[start_cell].column)
+
+
+	coords = {'x':0,'y':0}
+	
+	for row in range(row_start,row_start+row_count):
+		for col in range(col_start,col_start+col_count):
+
+			xlws.cell(row=row,column=col,value=lst[coords['y']][coords['x']])
+			coords['x'] += 1
+		coords['y'] += 1
+		coords['x'] = 0
+
+#
+	wb.save(save_dir+ rev_filename)
+	return rev_filename
 
 
